9dc740e9aa3758155c7bf0f19888b2b332d6115b
RedJuluNew <julianbraunkettwig@gmail.com>
2024-05-16 15:01:35 +0200

    Update script.js

--- dashboard/script.js
+++ dashboard/script.js
@@ -15,3 +15,16 @@ function logout() {
     alert('Du wurdest ausgeloggt!');
     // Hier könnte der Code stehen, um zur Login-Seite weiterzuleiten
 }
+
+
+// Überprüfung, ob der Benutzer eingeloggt ist
+if (!isLoggedIn()) {
+    // Wenn der Benutzer nicht eingeloggt ist, wird er zur 404-Seite weitergeleitet
+    window.location.href = '../404.html';
+}
+
+// Funktion zur Überprüfung, ob der Benutzer eingeloggt ist
+function isLoggedIn() {
+    // Überprüfen, ob im sessionStorage der Schlüssel 'loggedIn' auf 'true' gesetzt ist
+    return sessionStorage.getItem('loggedIn') === 'true';
+}
